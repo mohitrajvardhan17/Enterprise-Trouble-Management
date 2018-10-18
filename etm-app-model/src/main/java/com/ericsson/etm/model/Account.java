816df9d04b37b4e839df1e76fe38fec96e0138d3
emorajv <emorajv@5CD6454K26.ericsson.se>
2018-10-18 11:55:32 +0530

    Completed Account.java implementation

--- etm-app-model/src/main/java/com/ericsson/etm/model/Account.java
+++ etm-app-model/src/main/java/com/ericsson/etm/model/Account.java
@@ -7,6 +7,8 @@ package com.ericsson.etm.model;
 
 import java.io.Serializable;
 import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -34,34 +36,46 @@ class Account  implements Serializable{
 	@Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private int id;
-    private String description;
     @OneToMany
     @JoinTable(
-        name="user_role_relation",
-        joinColumns = @JoinColumn(name="user_id",referencedColumnName = "id"),
-        inverseJoinColumns = @JoinColumn(name="role_id",referencedColumnName = "id")
+        name="ACCOUNT_ROLE_RELATION",
+        joinColumns = @JoinColumn(name="ACCOUNT_ID",referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name="ROLE_ID",referencedColumnName = "id")
     )
     private Role role;
     @OneToMany
     @JoinTable(
-        name="user_permission_relation",
-        joinColumns = @JoinColumn(name="user_id",referencedColumnName = "id"),
-        inverseJoinColumns = @JoinColumn(name="permission_id",referencedColumnName = "id")
+        name="ACCOUNT_PERMISSION_RELATION",
+        joinColumns = @JoinColumn(name="ACCOUNT_ID",referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name="PERMISSION_ID",referencedColumnName = "id")
     )
     private Permission permission;
-    @JoinTable(
-        name="ACCOUNT_DEPARTMENT_RELATION",
-        joinColumns = @JoinColumn(name = "ACCOUNT_ID",referencedColumnName = "id"),
-        inverseJoinColumns = @JoinColumn(name = "DEPARTMENT_ID", referencedColumnName = "id")
-    )
-    private Department account;
+    @OneToMany    
     @JoinTable(
         name="ACCOUNT_TEAM_RELATION",
         joinColumns = @JoinColumn(name = "ACCOUNT_ID",referencedColumnName = "id"),
         inverseJoinColumns = @JoinColumn(name = "TEAM_ID", referencedColumnName = "id")
     )
     private Team team;
+    @OneToMany    
+    @JoinTable(
+        name="ACCOUNT_DEPARTMENT_RELATION",
+        joinColumns = @JoinColumn(name = "ACCOUNT_ID",referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "DEPARTMENT_ID", referencedColumnName = "id")
+    )
+    private Department department;
+    @OneToMany    
+    @JoinTable(
+        name="ACCOUNT_CONTACT_RELATION",
+        joinColumns = @JoinColumn(name = "ACCOUNT_ID",referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "CONTACT_ID", referencedColumnName = "id")
+    )
+    private Contact contact;
+    
+    private String description;
+    @Column(name = "is_active")
     private boolean isActive;
+    @Column(name = "is_default")
     private boolean isDefault;
     @UpdateTimestamp    
     @Temporal(javax.persistence.TemporalType.DATE)
@@ -74,5 +88,167 @@ class Account  implements Serializable{
     private Users lastModifiedBy;
     //@CreatedBy
     @OneToOne
-    private Users createdBy;    
+    private Users createdBy;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the role
+	 */
+	public Role getRole() {
+		return role;
+	}
+	/**
+	 * @param role the role to set
+	 */
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	/**
+	 * @return the permission
+	 */
+	public Permission getPermission() {
+		return permission;
+	}
+	/**
+	 * @param permission the permission to set
+	 */
+	public void setPermission(Permission permission) {
+		this.permission = permission;
+	}
+	/**
+	 * @return the team
+	 */
+	public Team getTeam() {
+		return team;
+	}
+	/**
+	 * @param team the team to set
+	 */
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+	/**
+	 * @return the department
+	 */
+	public Department getDepartment() {
+		return department;
+	}
+	/**
+	 * @param department the department to set
+	 */
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	/**
+	 * @return the contact
+	 */
+	public Contact getContact() {
+		return contact;
+	}
+	/**
+	 * @param contact the contact to set
+	 */
+	public void setContact(Contact contact) {
+		this.contact = contact;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the isActive
+	 */
+	public boolean isActive() {
+		return isActive;
+	}
+	/**
+	 * @param isActive the isActive to set
+	 */
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	/**
+	 * @return the isDefault
+	 */
+	public boolean isDefault() {
+		return isDefault;
+	}
+	/**
+	 * @param isDefault the isDefault to set
+	 */
+	public void setDefault(boolean isDefault) {
+		this.isDefault = isDefault;
+	}
+	/**
+	 * @return the lastModifiedDate
+	 */
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+	/**
+	 * @param lastModifiedDate the lastModifiedDate to set
+	 */
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+	/**
+	 * @return the creationDate
+	 */
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	/**
+	 * @param creationDate the creationDate to set
+	 */
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	/**
+	 * @return the lastModifiedBy
+	 */
+	public Users getLastModifiedBy() {
+		return lastModifiedBy;
+	}
+	/**
+	 * @param lastModifiedBy the lastModifiedBy to set
+	 */
+	public void setLastModifiedBy(Users lastModifiedBy) {
+		this.lastModifiedBy = lastModifiedBy;
+	}
+	/**
+	 * @return the createdBy
+	 */
+	public Users getCreatedBy() {
+		return createdBy;
+	}
+	/**
+	 * @param createdBy the createdBy to set
+	 */
+	public void setCreatedBy(Users createdBy) {
+		this.createdBy = createdBy;
+	}
+	/**
+	 * @return the serialversionuid
+	 */
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 }
